920a979ad3c3f307aa51b9907e582b65d9dee888
Admin <alexandre.kimtsaris@bstorm.be>
2021-03-15 13:38:05 +0100

    okidoki

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TestModule } from './test-module/test.module';
+import { BindingComponent } from './components/binding/binding.component';
+import { FormsModule } from '@angular/forms';
+import { MenuDemoComponent } from './components/menu-demo/menu-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    BindingComponent,
+    MenuDemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/binding/binding.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styleUrls: ['./binding.component.css']
+})
+export class BindingComponent implements OnInit {
+
+  nom = 'alexandre';
+  lienVers = 'http://www.google.com';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
